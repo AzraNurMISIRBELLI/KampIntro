77aeeb78ea71722e3eeab2d85e64a1e6e1a93d12
AzraNurMISIRBELLI <77413791+AzraNurMISIRBELLI@users.noreply.github.com>
2021-01-18 22:43:50 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Metotlar
+{
+	class Program
+	{   //Dont repaet  yourself -Dry- Clean code- Best Practice
+		static void Main(string[] args)
+		{
+			Product pr1= new Product();
+			pr1.Adi = "Elma";
+			pr1.Fiyati = 15;
+			pr1.Aciklama = "Amasya Elması";
+
+			Product pr2 = new Product();
+			pr2.Adi = "Karpuz";
+			pr2.Fiyati = 80;
+			pr2.Aciklama = "Diyarbakır Karpuzu";
+
+			Product[] urunler = new Product[] { pr1,pr2};
+			foreach (var product in urunler)
+			{
+				Console.WriteLine(product.Adi);
+				Console.WriteLine(product.Fiyati);
+				Console.WriteLine(product.Aciklama);
+				Console.WriteLine("------------------");
+
+
+
+
+			}
+
+			Console.WriteLine("----MEtodlar-------");
+			SepetManager sepetManager = new SepetManager();
+			sepetManager.Ekle(pr1);
+			sepetManager.Ekle(pr2);
+
+			sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 80,10);
+			sepetManager.Ekle2("Elma", "Yeşil Elma", 150,3);
+			sepetManager.Ekle2("Elma", "Kırmızı Elma ", 100,5);
+
+
+		}
+	}
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+	class SepetManager
+	{
+		public void Ekle(Product urun)
+		{
+			Console.WriteLine("Tebrikler. Sepete Eklendi:" + urun.Adi);
+
+		}
+		public void Ekle2(string urunAdi,string urunAciklama,double fiyat,int stok)
+		{
+			Console.WriteLine("Tebrikler .Sepete Eklendi:" + urunAdi);
+
+		}
+	}
+}
