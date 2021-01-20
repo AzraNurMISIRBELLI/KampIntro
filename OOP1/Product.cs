1fdc687ba5ee67514ec1284a1b56fbb19530c0c7
AzraNurMISIRBELLI <77413791+AzraNurMISIRBELLI@users.noreply.github.com>
2021-01-20 23:50:40 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{//snippet=hazırkodlar-->prop tab tab
+	class Product
+	{
+		public int Id { get; set; }//Primary Key
+
+		public int CategoryId { get; set; }  //Referans Foreign Key
+		public string ProductName { get; set; }
+
+		public double UnitPrice { get; set; } //Fiyat
+
+		public int UnitInStock { get; set; }  //Stok
+
+		//CRUD Operasyonları
+
+			
+	}
+}
